7f9e7722a8ff1a29670b155ab380dde34e980ffd
tracer-nju <tracer@nju.edu.cn>
2020-12-02 22:45:48 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: f72213ddc7dd41febf337d186af36ac9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  22:45:48 up  3:19,  1 user,  load average: 1.02, 0.92, 0.90

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -14,11 +14,10 @@ int *sieve(int n) {
     is_prime[i] = true;
 
   for (int i = 2; i <= n; i++) {
-    if(is_prime[i] == false){
-      continue;
-    }
-    for (int j = i + i; j <= n; j += i) {
-      is_prime[j] = false;
+    if(is_prime[i] == true){
+      for (int j = i + i; j <= n; j += i) {
+        is_prime[j] = false;
+      }
     }
   }
 
