75cf8b94c502a4667d5a60bf6766cab4fcd8cf65
tracer-nju <tracer@nju.edu.cn>
2020-12-02 22:35:56 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: f72213ddc7dd41febf337d186af36ac9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  22:35:56 up  3:09,  1 user,  load average: 1.88, 1.41, 0.94

--- perftune/main.c
+++ perftune/main.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 
 int *sieve(int n);
 
 int main() {
   int *primes = sieve(400);
+  printf("%d s", (double)clock()/CLOCKS_PER_SEC);
   for (int *cur = primes, i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);
     if (i % 8 == 7 || !*(cur + 1)) printf("\n");
