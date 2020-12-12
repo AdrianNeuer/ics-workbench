1982f26b03a867d0368ce067104a3ff8102e0f1c
tracer-nju <tracer@nju.edu.cn>
2020-12-12 18:07:51 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: 0e147050b4e544ceadd3963964404904   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:07:51 up 1 day,  2:46,  1 user,  load average: 0.57, 0.53, 0.67

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -8,7 +8,7 @@
 static bool is_prime[N];
 static int  primes[N];
 
-int sqrt (int n){
+int st (int n){
   for (int i = 2; i <= n; i++){
     if (i*i > n){
       return i -1;
@@ -20,7 +20,7 @@ int *sieve(int n) {
   for (int i = 3; i <= n; i += 2)
     is_prime[i] = true;
 
-  for (int i = 3; i <= sqrt(n); i+=2) {
+  for (int i = 3; i <= st(n); i+=2) {
     if(is_prime[i]){
       for (int j = i + (i << 1); j <= n; j += (i << 1)) {
         is_prime[j] = false;
