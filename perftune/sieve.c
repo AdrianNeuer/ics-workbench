8d7ef19e3b6a8365b7514ee7b1be9ca1ae95497b
tracer-nju <tracer@nju.edu.cn>
2020-12-12 18:07:28 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: 0e147050b4e544ceadd3963964404904   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:07:28 up 1 day,  2:46,  1 user,  load average: 0.86, 0.58, 0.69

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -2,18 +2,25 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
-#include <math.h>
 
 #define N 10000000
 
 static bool is_prime[N];
 static int  primes[N];
 
+int sqrt (int n){
+  for (int i = 2; i <= n; i++){
+    if (i*i > n){
+      return i -1;
+    }
+  }
+}
+
 int *sieve(int n) {
   for (int i = 3; i <= n; i += 2)
     is_prime[i] = true;
 
-  for (int i = 3; i <= n/6; i+=2) {
+  for (int i = 3; i <= sqrt(n); i+=2) {
     if(is_prime[i]){
       for (int j = i + (i << 1); j <= n; j += (i << 1)) {
         is_prime[j] = false;
