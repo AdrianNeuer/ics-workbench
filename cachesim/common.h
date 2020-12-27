681f3fb70866d8ebb3e16b88399ec62b886bac83
tracer-nju <tracer@nju.edu.cn>
2020-12-27 20:21:42 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: a0040202bc7945a2a132a7ce24723b0b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  20:21:42 up 1 day,  2:06,  1 user,  load average: 0.95, 0.33, 0.11

--- cachesim/common.h
+++ cachesim/common.h
@@ -7,11 +7,11 @@
 #include <assert.h>
 
 #define exp2(x) (1 << (x))
-#define mask_with_len(x) (exp2(x) - 1)
+#define mask_with_len(x) (exp2(x) - 1)  // 2^x-1
 
 #define MEM_SIZE (1 << 25) // 1MB
 #define BLOCK_WIDTH  6  // 64B
-#define BLOCK_SIZE exp2(BLOCK_WIDTH)
+#define BLOCK_SIZE exp2(BLOCK_WIDTH)  // 1 << 6 = 64
 
 typedef uint8_t bool;
 #define true 1
