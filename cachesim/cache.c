dcfe60fa208f02dabb11ea3744284dab2f2444aa
tracer-nju <tracer@nju.edu.cn>
2021-01-09 16:13:07 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: a90c2a0e054945649e92aafd4f643569   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  16:13:07 up 23:18,  1 user,  load average: 0.28, 0.43, 0.40

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -14,7 +14,7 @@ typedef struct
   uint32_t tag;
 }  Cache_block;
 
-Cache_block Cache[64][4];  // 4*64*64 = 16KB
+Cache_block Cache[64][4];  // 4*64*64 = 16KB  size可以修改，此处为了适配main.c中的初始化
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
@@ -43,7 +43,7 @@ uint32_t cache_read(uintptr_t addr) {
     for (int i = 0; i < 4; i++){
       if(Cache[group_num][i]. valid_bit == false){
         empty = true;
-        mem_read(addr << 6, Cache[group_num][i].Block);
+        mem_read(addr >> 6, Cache[group_num][i].Block);
         Cache[group_num][i].tag = tag;
         Cache[group_num][i].valid_bit = true;
         Cache[group_num][i].dirty_bit = false;
@@ -56,7 +56,7 @@ uint32_t cache_read(uintptr_t addr) {
       if (Cache[group_num][chose_place].dirty_bit == true){
         mem_write(Cache[group_num][chose_place].tag << 6 | group_num, Cache[group_num][chose_place].Block);
       }
-      mem_read(addr << 6, Cache[group_num][chose_place].Block);
+      mem_read(addr >> 6, Cache[group_num][chose_place].Block);
       Cache[group_num][chose_place].tag = tag;
       Cache[group_num][chose_place].valid_bit = true;
       Cache[group_num][chose_place].dirty_bit = false;
@@ -75,11 +75,52 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
   uint32_t block_num = addr & 0x3f;  // 内存块内地址
 
   for (int i = 0; i < 4; i++){
-    if (Cache[group_num][i].tag == tag && Cache[group_num][i].valid_bit == true){}
+    if (Cache[group_num][i].tag == tag && Cache[group_num][i].valid_bit == true){
+      is_hit = true;
+      uint32_t *place = (void *)(Cache[group_num][i].Block);
+      *place = (*place & ~wmask) | (data & wmask);
+    }
+  }
+  if (is_hit == false){
+    empty = false;
+    for (int i = 0; i < 4; i++){
+      if(Cache[group_num][i]. valid_bit == false){
+        empty = true;
+        mem_read(addr >> 6, Cache[group_num][i].Block);
+        uint32_t *place = (void *)(Cache[group_num][i].Block);
+        *place = (*place & ~wmask) | (data & wmask);
+        Cache[group_num][i].tag = tag;
+        Cache[group_num][i].valid_bit = true;
+        Cache[group_num][i].dirty_bit = false;
+        mem_write(addr >> 6, Cache[group_num][i].Block);
+        break;
+      }
+    }
+    if (empty == false){
+      int chose_place = rand() % 4;
+      if (Cache[group_num][chose_place].dirty_bit == true){
+        mem_write(Cache[group_num][chose_place].tag << 6 | group_num, Cache[group_num][chose_place].Block);
+      }
+      mem_read(addr >> 6, Cache[group_num][chose_place].Block);
+      uint32_t *place = (void *)(Cache[group_num][chose_place].Block);
+      *place = (*place & ~wmask) | (data & wmask);
+      Cache[group_num][chose_place].tag = tag;
+      Cache[group_num][chose_place].valid_bit = true;
+      Cache[group_num][chose_place].dirty_bit = false;
+      mem_write(addr >> 6, Cache[group_num][chose_place].Block);
+    }
   }
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  int tol_size = 1 << total_size_width;  //  cache总大小
+  int association = 1 << associativity_width;  //  cache关联度大小，也即每组行数
+
+  for(int i = 0; i < tol_size / BLOCK_SIZE / association; i ++)
+    for (int j = 0; j < association; j ++){
+      Cache[i][j].valid_bit = false;
+      Cache[i][j].dirty_bit = false;
+    }
 }
 
 void display_statistic(void) {
