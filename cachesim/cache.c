29f3f98cd2d98a66754451116def28dc7f521f4e
tracer-nju <tracer@nju.edu.cn>
2021-01-09 11:07:23 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: a90c2a0e054945649e92aafd4f643569   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:07:23 up 18:12,  1 user,  load average: 1.13, 0.53, 0.38

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -1,6 +1,5 @@
 #include "common.h"
 #include <inttypes.h>
-#include <time.h>
 
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
@@ -9,8 +8,8 @@ static uint64_t cycle_cnt = 0;
 
 typedef struct 
 {
-  int valid_bit;  // 1合法， 0不合法
-  int dirty_bit;  // 1修改， 0未修改
+  bool valid_bit;  // true合法， false不合法
+  bool dirty_bit;  // true修改， false未修改
   uint8_t Block[64];  // 64B
   uint32_t tag;
 }  Cache_block;
@@ -22,6 +21,41 @@ void cycle_increase(int n) { cycle_cnt += n; }
 // TODO: implement the following functions
 
 uint32_t cache_read(uintptr_t addr) {
+  bool is_hit = false;
+  bool empty;
+
+  uint32_t tag = (addr >> 12) & 0xff;  // 标记
+  uint32_t group_num = (addr >> 6) & 0x3f;  // 组号
+  uint32_t block_num = addr & 0x3f;  // 内存块地址
+
+  uint32_t *ret;
+
+  for (int i = 0; i < 4; i++){
+    if (Cache[group_num][i].tag == tag && Cache[group_num][i].valid_bit == true){
+      ret = (void *)(Cache[group_num][i].Block);
+      is_hit = true;
+      break;
+    }
+  }
+
+  if (is_hit == false){
+    empty = false;
+    for (int i = 0; i < 4; i++){
+      if(Cache[group_num][i]. valid_bit == false){
+        empty = true;
+        mem_read(addr, Cache[group_num][i].Block);
+        Cache[group_num][i].tag = tag;
+        Cache[group_num][i].valid_bit = true;
+        Cache[group_num][i].dirty_bit = false;
+        ret = (void *) Cache[group_num][i].Block;
+        break;
+      }
+    }
+    if (empty == true){
+      int chose_place = rand() % 4;
+    }
+  }
+
   return 0;
 }
 
