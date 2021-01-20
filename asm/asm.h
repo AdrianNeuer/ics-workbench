a09b56a3f1032fc7cfe200acc5c1f42615c79eb2
tracer-nju <tracer@nju.edu.cn>
2021-01-20 16:43:47 +0800

    Static hostname: adrian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 59725fff0e5a4c31a2c149a8b177ddd3            Boot ID: 223d9045ae8a41f389c382b7c8037b97   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  16:43:47 up  6:24,  1 user,  load average: 1.04, 0.67, 0.77

--- asm/asm.h
+++ asm/asm.h
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 // TODO: replace with your definition
-#include <setjmp.h> // remove this
+//#include <setjmp.h> // remove this
 #define asm_jmp_buf jmp_buf
 
 int64_t asm_add(int64_t a, int64_t b);
